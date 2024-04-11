4f9c5a2553cb2ecc98337994f1fcaf6a3cea25a3
Ankush263 <ankushbanik263@gmail.com>
2024-04-11 09:55:13 +0530

    initial commit

---
+++ src/app/snippets/[id]/loading.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function SnippetShowLoadingPage() {
+	return <div>Loading...</div>;
+}
+
+export default SnippetShowLoadingPage;
---
+++ src/app/snippets/[id]/not-found.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function SnippetNotFound() {
+	return (
+		<div>
+			<h1 className="text-xl bold">
+				{`Sorry, we couldn't find that perticular snippet`}
+			</h1>
+		</div>
+	);
+}
+
+export default SnippetNotFound;
