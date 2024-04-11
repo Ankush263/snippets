7ecfae266cc24df2c90842821ae115efce287456
Ankush263 <ankushbanik263@gmail.com>
2024-04-11 12:23:27 +0530

    Add Error handling

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -18,3 +18,47 @@ export async function deleteSnipt(id: number) {
 
 	redirect('/');
 }
+
+export async function createSnippet(
+	formState: { message: string },
+	formData: FormData
+) {
+	try {
+		// Chack the user's inputs and make sure they're valid
+		const title = formData.get('title');
+		const code = formData.get('code');
+
+		if (typeof title !== 'string' || title.length < 3) {
+			return {
+				message: 'Title must be longer',
+			};
+		}
+
+		if (typeof code !== 'string' || code.length < 10) {
+			return {
+				message: 'Code must be longer',
+			};
+		}
+
+		// Create a new recoard in the database
+		await db.snippet.create({
+			data: {
+				title,
+				code,
+			},
+		});
+	} catch (error: unknown) {
+		if (error instanceof Error) {
+			return {
+				message: error.message,
+			};
+		} else {
+			return {
+				message: 'Something went wrong...',
+			};
+		}
+	}
+
+	// Redirect the user back to root route
+	redirect('/');
+}
